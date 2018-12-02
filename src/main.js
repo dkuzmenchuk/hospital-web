7c5ddd52f1aaed675761b22e4733ac1ce392405a
Denis Kuzmenchuk <denis.kuzm@tut.by>
2018-12-02 13:09:32 +0300

    Create login page markdown

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
 
+Vue.use(Vuetify)
 Vue.config.productionTip = false
+
+import 'vuetify/dist/vuetify.min.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-content>
+        <v-container fluid fill-height>
+            <v-layout align-center justify-center>
+                <v-flex xs12 sm8 md4>
+                    <v-card class="elevation-12">
+                        <v-toolbar dark color="primary">
+                            <v-toolbar-title>Login form</v-toolbar-title>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-text-field prepend-icon="person" name="username" label="Username" type="text" v-model="credentials.username"></v-text-field>
+                            <v-text-field prepend-icon="lock" name="password" label="Password" id="password" type="password" v-model="credentials.password"></v-text-field>
+                        </v-card-text>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="primary"  @click.native="login">Login</v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-flex>
+            </v-layout>
+        </v-container>
+    </v-content>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        credentials: {
+          username: '',
+          password: ''
+        }
+      };
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
