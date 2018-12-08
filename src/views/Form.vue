04d78b4c6d28a3f38cf0a5de2b4145792e6f5ab1
Denis Kuzmenchuk <denis.kuzm@tut.by>
2018-12-08 19:37:31 +0300

    Add different types of service

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -5,7 +5,7 @@
                 <v-flex xs12 sm8 md4>
                     <v-card class="elevation-12">
                         <v-toolbar dark color="primary">
-                            <v-toolbar-title>SPECIALIZATION</v-toolbar-title>
+                            <v-toolbar-title>Specialization</v-toolbar-title>
                         </v-toolbar>
                         <v-card-text>
                            <v-select
@@ -17,7 +17,7 @@
                     </v-card>
                     <v-card class="elevation-12">
                         <v-toolbar dark color="primary">
-                            <v-toolbar-title>DOCTOR</v-toolbar-title>
+                            <v-toolbar-title>Doctor</v-toolbar-title>
                         </v-toolbar>
                         <v-card-text>
                             <v-select
@@ -29,20 +29,21 @@
                     </v-card>
                     <v-card class="elevation-12">
                         <v-toolbar dark color="primary">
-                            <v-toolbar-title>DATE</v-toolbar-title>
+                            <v-toolbar-title>Date</v-toolbar-title>
                         </v-toolbar>
                         <v-card-text>
                             <v-date-picker
                                     v-model="form.date"
                                     full-width
                                     landscape
+                                    :min="minDateOfVisit"
                                     class="mt-3"
                             ></v-date-picker>
                         </v-card-text>
                     </v-card>
                     <v-card class="elevation-12">
                         <v-toolbar dark color="primary">
-                            <v-toolbar-title>TIME</v-toolbar-title>
+                            <v-toolbar-title>Time</v-toolbar-title>
                         </v-toolbar>
                         <v-card-text>
                             There will be time picker
@@ -50,7 +51,7 @@
                     </v-card>
                     <v-card class="elevation-12">
                         <v-toolbar dark color="primary">
-                            <v-toolbar-title>PERSONAL DATA</v-toolbar-title>
+                            <v-toolbar-title>Personal data</v-toolbar-title>
                         </v-toolbar>
                         <v-card-text>
                             <v-text-field
@@ -83,7 +84,11 @@
                                         label="Date of Birth"
                                         readonly
                                 ></v-text-field>
-                                <v-date-picker v-model="form.dob" scrollable>
+                                <v-date-picker
+                                        :max="maxDobDate"
+                                        v-model="form.dob"
+                                        scrollable
+                                >
                                     <v-spacer></v-spacer>
                                     <v-btn flat color="primary" @click="dobModal = false">Cancel</v-btn>
                                     <v-btn flat color="primary" @click="$refs.dobDialog.save(form.dob)">OK</v-btn>
@@ -116,6 +121,111 @@
                             ></v-text-field>
                         </v-card-text>
                     </v-card>
+                    <v-card class="elevation-12">
+                        <v-toolbar dark color="primary">
+                            <v-toolbar-title>Service form</v-toolbar-title>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-select
+                                    :items="serviceForms"
+                                    item-text="text"
+                                    item-value="value"
+                                    label="Service form"
+                                    v-model="form.service"
+                            ></v-select>
+                        </v-card-text>
+                    </v-card>
+                    <v-card class="elevation-12"
+                            v-if="isInsuranceService"
+                    >
+                        <v-toolbar dark color="primary">
+                            <v-toolbar-title>Insurance details</v-toolbar-title>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-select
+                                    :items="insuranceCompanies"
+                                    label="Insurance Company"
+                                    v-model="form.insuranceCompany"
+                            ></v-select>
+                            <v-text-field
+                                    label="Insurance policy number"
+                                    type="text"
+                                    v-model="form.insurancePolicyNumber"
+                            ></v-text-field>
+                            <v-dialog
+                                    ref="insurancePolicyStartDate"
+                                    v-model="insurancePolicyStartDateModal"
+                                    :return-value.sync="form.insurancePolicyStartDate"
+                                    persistent
+                                    lazy
+                                    full-width
+                                    width="290px"
+                            >
+                                <v-text-field
+                                        slot="activator"
+                                        v-model="form.insurancePolicyStartDate"
+                                        label="Insurance Policy Start Date"
+                                        readonly
+                                ></v-text-field>
+                                <v-date-picker v-model="form.insurancePolicyStartDate" scrollable>
+                                    <v-spacer></v-spacer>
+                                    <v-btn flat color="primary" @click="insurancePolicyStartDateModal = false">Cancel</v-btn>
+                                    <v-btn flat color="primary" @click="$refs.insurancePolicyStartDate.save(form.insurancePolicyStartDate)">OK</v-btn>
+                                </v-date-picker>
+                            </v-dialog>
+                            <v-dialog
+                                    ref="insurancePolicyEndDate"
+                                    v-model="insurancePolicyEndDateModal"
+                                    :return-value.sync="form.insurancePolicyEndDate"
+                                    persistent
+                                    lazy
+                                    full-width
+                                    width="290px"
+                            >
+                                <v-text-field
+                                        slot="activator"
+                                        v-model="form.insurancePolicyEndDate"
+                                        label="Insurance Policy End Date"
+                                        readonly
+                                ></v-text-field>
+                                <v-date-picker
+                                        :min="insurancePolicyEndDateMin"
+                                        v-model="form.insurancePolicyEndDate"
+                                        scrollable
+                                >
+                                    <v-spacer></v-spacer>
+                                    <v-btn flat color="primary" @click="insurancePolicyEndDateModal = false">Cancel</v-btn>
+                                    <v-btn flat color="primary" @click="$refs.insurancePolicyEndDate.save(form.insurancePolicyEndDate)">OK</v-btn>
+                                </v-date-picker>
+                            </v-dialog>
+                        </v-card-text>
+                    </v-card>
+                    <v-card class="elevation-12"
+                            v-if="isContractService"
+                    >
+                        <v-toolbar dark color="primary">
+                            <v-toolbar-title>Contract details</v-toolbar-title>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-text-field
+                                    label="Enterprise name"
+                                    type="text"
+                                    v-model="form.contractEnterpriseName"
+                            ></v-text-field>
+                        </v-card-text>
+                    </v-card>
+                    <v-card class="elevation-12">
+                        <v-toolbar dark color="primary">
+                            <v-toolbar-title>Notes</v-toolbar-title>
+                        </v-toolbar>
+                        <v-card-text>
+                            <v-textarea
+                                    auto-grow
+                                    label="Notes"
+                                    v-model="form.notes"
+                            ></v-textarea>
+                        </v-card-text>
+                    </v-card>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -123,7 +233,7 @@
 </template>
 
 <script>
-  import moment from 'moment';
+  import moment from 'moment'
 
   export default {
     name: 'Form',
@@ -146,13 +256,34 @@
           firstName: '',
           lastName: '',
           middleName: '',
-          dob: moment().format('YYYY-MM-DD'),
+          dob: null,
           nationality: null,
           gender: null,
           address: '',
           phone: '',
           email: '',
+          service: null,
+          notes: '',
+          insuranceCompany: null,
+          insurancePolicyNumber: '',
+          insurancePolicyStartDate: null,
+          insurancePolicyEndDate: null,
+          contractEnterpriseName: ''
         },
+        insuranceCompanies: [
+          'Company 1',
+          'Company 2',
+          'Company 3',
+          'Company 4',
+          'Company 5',
+        ],
+        insurancePolicyEndDateModal: false,
+        insurancePolicyStartDateModal: false,
+        serviceForms: [
+          {text: 'Paid', value: 'paid'},
+          {text: 'Medical insurance', value: 'insurance'},
+          {text: 'Under contract with the enterprise', value: 'contract'},
+        ],
         specializations: [
             'Abdominal Radiology Radiology-Diagnostic',
             'Addiction Psychiatry Psychiatry',
@@ -172,7 +303,54 @@
         ]
       }
     },
+    computed: {
+      insurancePolicyEndDateMin: function () {
+        return this.form.insurancePolicyStartDate === null
+          ? undefined
+          : moment(this.form.insurancePolicyStartDate).add(1, 'day').format('YYYY-MM-DD')
+      },
+      isInsuranceService: function () {
+        return this.form.service === 'insurance'
+      },
+      isContractService: function () {
+        return this.form.service === 'contract'
+      },
+      maxDobDate: function () {
+        return moment().format('YYYY-MM-DD')
+      },
+      minDateOfVisit: function () {
+        return moment().format('YYYY-MM-DD')
+      }
+    },
+    watch: {
+      'form.service': function (currentService) {
+        switch (currentService) {
+          case 'paid': {
+            this.resetContractInformation();
+            this.resetInsuranceInformation();
+            break;
+          }
+          case 'insurance': {
+            this.resetContractInformation();
+            break;
+          }
+          case 'contract': {
+            this.resetInsuranceInformation();
+            break;
+          }
+        }
+      }
+    },
     methods: {
+      resetContractInformation () {
+        this.form.contractEnterpriseName = ''
+      },
+      resetInsuranceInformation () {
+        this.form.insuranceCompany = null;
+        this.form.insurancePolicyNumber = '';
+        this.form.insurancePolicyStartDate = null;
+        this.form.insurancePolicyEndDate = null
+      },
       apply () {
         return true
       }
